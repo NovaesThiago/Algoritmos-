a6c032f489316e0e8116c326c39f2f03323f41fe
Thiago Novaes <novaesthiagoss@gmail.com>
2024-11-27 20:07:41 -0300

    Add files via upload

---
+++ CAP6/q1.js
@@ -0,0 +1,26 @@
+let a
+
+function saidaDeDados(){
+    console.log("Hello World")
+}
+
+function entradaDeDados(){
+    a = prompt("Digite uma palavra: ")
+}
+
+function numeroInteiro(){
+    a = parseInt
+    console.log(a)
+}
+
+function numeroFloat(){
+    a = parseFloat
+    console.log(a)
+}
+
+function diaDoMes(){
+   let dia = new Date().getDate()
+   console.log(dia)
+}
+
+saidaDeDados()
